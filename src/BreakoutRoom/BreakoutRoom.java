2f21ecc14df82dcc877f0f4e74083a9e4f27ffab
eddgru6 <eddgru-6@student.ltu.se>
2022-04-05 10:31:09 +0200

    La till // Hello there

--- src/BreakoutRoom/BreakoutRoom.java
+++ src/BreakoutRoom/BreakoutRoom.java
@@ -1,7 +1,8 @@
 package BreakoutRoom;
 
 public class BreakoutRoom {
-    public static void main(String[] args) {
+    public static <String> void main(String[] args) {
         System.out.println("Hello World");
     }
+    // Hello there
 }
